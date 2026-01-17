73b96f1f1e8bb4eeb74c056d58111d7087528b9c
Isaiah Castillo <isaiahcastillo285@gmail.com>
2026-01-16 19:50:35 -0800

    #add.Console.WriteLine("Hello, World!");

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,8 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string[]strings)
         {
             Console.WriteLine("Hello, World!!!");
         }
